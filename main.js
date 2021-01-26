ba2b8ed1c4bdbc91abc6b2f3fd41aacc01279b59
pooh <cktjdgus45@naver.com>
2021-01-26 15:58:52 +0900

    JS :fade Effect scroll

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
 const nav = document.querySelector('#navbar');
-const contact = document.querySelector('#contact');
-const contactBtn = document.querySelector('.home__contact');
+const homeContent = document.querySelector('.home__container');
+const contactBtn = homeContent.querySelector('.home__contact');
 const navMenu = document.querySelector('.navbar__menu');
 
-const navHeight = nav.getBoundingClientRect().bottom;
 
-window.addEventListener('scroll', (event) => {
+document.addEventListener('scroll', () => {
     const currentScrollPos = window.scrollY;
+    changeNav(currentScrollPos);
+    changeHome(currentScrollPos);
+})
+
+//change nav bgColor when scroll nav
+
+function changeNav(currentScrollPos) {
+    const navHeight = nav.getBoundingClientRect().bottom;
     if (currentScrollPos > navHeight) {
         nav.classList.add('light-color');
     } else {
         nav.classList.remove('light-color');
     }
-})
+}
+//fade away slowly when scroll home
+
+function changeHome(currentScrollPos) {
+    const homeHeight = homeContent.getBoundingClientRect().bottom;
+    homeContent.style.opacity = 1 - (currentScrollPos / homeHeight);
+}
 
 //scroll to section when clicked.
 
 navMenu.addEventListener('click', (event) => {
     const link = event.target.dataset.link;
     if (link) {
-        const scrollTo = document.querySelector(link);
-        scrollTo.scrollIntoView({ behavior: "smooth" });
+        scrollIntoView(link);
     }
 })
 
 contactBtn.addEventListener('click', () => {
-    contact.scrollIntoView({ behavior: "smooth" });
+    scrollIntoView('#contact');
 })
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
