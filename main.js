34b0aecde87e973348251e6982cbc10505384c42
pooh <cktjdgus45@naver.com>
2021-01-29 17:03:36 +0900

    JS : active nav when in section

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const navMenu = document.querySelector('.navbar__menu');
 const nav = document.querySelector('#navbar');
 const homeContent = document.querySelector('.home__container');
 
@@ -9,6 +9,52 @@ document.addEventListener('scroll', () => {
     handleHomeScroll();
     showReturnBtn();
 })
+
+//active navItem when position at section
+const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#contact'];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navList = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2
+}
+
+let selectedNavItem = navList[1];
+let selectedIndex = 0;
+
+function activeOnOff(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            if (entry.boundingClientRect.y < 0) {
+                selectedIndex = index + 1;
+            } else {
+                selectedIndex = index - 1;
+            }
+        }
+    })
+};
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedIndex = 0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+        selectedIndex = navList.length - 1;
+    }
+    activeOnOff(navList[selectedIndex]);
+})
+
+const observer = new IntersectionObserver(callback, options);
+sections.forEach(section => observer.observe(section));
 
 //change nav bgColor when scroll nav
 
@@ -31,7 +77,7 @@ function handleHomeScroll() {
 const returnBtn = document.querySelector('.return-button');
 
 returnBtn.addEventListener('click', (event) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+    scrollIntoView('#home');
 })
 
 function showReturnBtn() {
@@ -43,8 +89,6 @@ function showReturnBtn() {
 }
 
 //scroll to section when clicked.
-const navMenu = document.querySelector('.navbar__menu');
-const navItems = navMenu.querySelectorAll('.navbar__menu__item');
 
 navMenu.addEventListener('click', (event) => {
     const link = event.target.dataset.link;
@@ -53,6 +97,7 @@ navMenu.addEventListener('click', (event) => {
     }
     navMenu.classList.remove('open');
     scrollIntoView(link);
+    activeOnOff(event.target);
 })
 
 const contactBtn = homeContent.querySelector('.home__contact');
@@ -65,6 +110,7 @@ contactBtn.addEventListener('click', () => {
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" });
+    activeOnOff(navList[sectionIds.indexOf(selector)]);
 }
 
 //Filter work
